d75e4ba63e0eec77386999f6f6f11aaaff155aaa
Imre Cserháti <cserhati.imre.andras@gmail.com>
2020-12-13 17:50:47 +0100

    hello

---
+++ src/components/CardList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Card from './Card';
+
+const CardList = ({ robots }) => {
+  if (true) {
+    throw new Error('NOOOOOOOOOOO');
+  }
+  return (
+    <div>
+      {robots.map(robot => {
+        return (
+          <Card
+            id={robot.id}
+            name={robot.name}
+            email={robot.email}
+            key={robot.id}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default CardList;
